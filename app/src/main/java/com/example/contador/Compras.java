b4302efdb79c174876b0ccedd92b967c25229488
AbelAlonso98 <abelalonsojimenez@gmail.com>
2023-10-19 17:23:54 +0200

    Shop mod added

---
+++ app/src/main/java/com/example/contador/Compras.java
@@ -0,0 +1,124 @@
+package com.example.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+
+public class Compras extends AppCompatActivity {
+
+    BigInteger num;
+    BigInteger inc = new BigInteger("1");
+    BigInteger incAuto = new BigInteger("1");
+    int tiempoAutoClick = 1000;
+
+    Button botonSumaTotal;
+    Button botonSumarAutoTotal;
+    Button botonSumarAutoSpeedTotal;
+
+    TextView textValorClick;
+    TextView textValorAutoClick;
+    TextView textVelocidadAutoClick;
+    TextView textMoneyCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compras);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            num = new BigInteger(extras.getString("MONEY_COUNT"));
+            inc = new BigInteger(extras.getString("CLICK_VALUE"));
+            incAuto = new BigInteger(extras.getString("AUTOCLICK_VALUE"));
+            tiempoAutoClick = extras.getInt("AUTOCLICK_TIME");
+        }
+
+
+        botonSumaTotal = (Button) findViewById(R.id.botonSumarTotal);
+        botonSumarAutoTotal = (Button) findViewById(R.id.botonSumarAutoTotal);
+        botonSumarAutoSpeedTotal = (Button) findViewById(R.id.botonSumarAutoSpeedTotal);
+        textValorClick = (TextView) findViewById(R.id.textValorClick);
+        textValorAutoClick = (TextView) findViewById(R.id.textValorAutoClick);
+        textVelocidadAutoClick = (TextView) findViewById(R.id.textVelocidadAutoClick);
+        textMoneyCount = (TextView) findViewById(R.id.txtMoneyCount);
+        setContText();
+    }
+
+    public void volverAJugar(View v) {
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("MONEY_COUNT", num.toString());
+        i.putExtra("CLICK_VALUE", inc.toString());
+        i.putExtra("AUTOCLICK_VALUE", incAuto.toString());
+        i.putExtra("AUTOCLICK_TIME", tiempoAutoClick);
+        startActivity(i);
+    }
+
+    public void setContText() {
+        botonSumaTotal.setText("100$ x" + num.divide(BigInteger.valueOf(100)));
+        botonSumarAutoTotal.setText("200$ x" + num.divide(BigInteger.valueOf(200)));
+        botonSumarAutoSpeedTotal.setText("400$ x" + num.divide(BigInteger.valueOf(400)));
+
+        textValorClick.setText("Click: " + inc.toString());
+        textValorAutoClick.setText("Autoclick: " + incAuto.toString());
+        textVelocidadAutoClick.setText("Autoclick speed: " + tiempoAutoClick + "m" + "s");
+
+        textMoneyCount.setText(num.toString());
+    }
+
+    public void sumar(View v) {
+        num = num.add(inc);
+        setContText();
+    }
+
+    public void sumaTotal(View v) {
+        // inc += num/100 and num=num%100
+        inc = inc.add(num.divide(BigInteger.valueOf(100)));
+        num = num.remainder(BigInteger.valueOf(100));
+        setContText();
+    }
+
+    public void sumaTotalAuto(View v) {
+        incAuto = incAuto.add(num.divide(BigInteger.valueOf(200)));
+        num = num.remainder(BigInteger.valueOf(200));
+        setContText();
+
+    }
+
+    public void incrementarAutoSpeed(View v) {
+        if (num.longValue() >= 400) {
+            tiempoAutoClick = (int) (tiempoAutoClick / 1.5);
+            num = num.subtract(BigInteger.valueOf(400));
+            setContText();
+        }
+    }
+
+    public void incrementarAutoSpeedTotal(View v) {
+        if (num.longValue() >= 400) {
+            int times = num.divide(BigInteger.valueOf(400)).intValue();
+            tiempoAutoClick = (int) (tiempoAutoClick / (1.5 * times));
+            num = num.remainder(BigInteger.valueOf(400));
+            setContText();
+        }
+    }
+
+    public void incrementar(View v) {
+        if (num.longValue() >= 100) {
+            inc = inc.add(BigInteger.valueOf(1));
+            num = num.subtract(BigInteger.valueOf(100));
+            setContText();
+        }
+    }
+
+    public void incrementarAuto(View v) {
+        if (num.longValue() >= 200) {
+            incAuto = incAuto.add(BigInteger.valueOf(1));
+            num = num.subtract(BigInteger.valueOf(200));
+            setContText();
+        }
+    }
+}
--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -19,16 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     TextView contador;
     ImageView moneda;
-    Button botonSumaTotal;
-    Button botonSumarAutoTotal;
-    Button botonSumarAutoSpeedTotal;
+
     TextView textValorClick;
     TextView textValorAutoClick;
     TextView textVelocidadAutoClick;
     ScaleAnimation fade_in = new ScaleAnimation(0.7f, 1.2f, 0.7f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 
 
-    BigInteger num = new BigInteger("0");
+    BigInteger num = new BigInteger("500");
     BigDecimal numD = new BigDecimal("0");
     BigInteger inc = new BigInteger("1");
     BigInteger incAuto = new BigInteger("1");
@@ -42,13 +40,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            num = new BigInteger(extras.getString("MONEY_COUNT"));
+            inc = new BigInteger(extras.getString("CLICK_VALUE"));
+            incAuto = new BigInteger(extras.getString("AUTOCLICK_VALUE"));
+            tiempoAutoClick = extras.getInt("AUTOCLICK_TIME");
+        }
 
         //  Cargo todos los componentes que voy a usar.
         contador = (TextView) findViewById(R.id.textocontador);
         moneda = (ImageView) findViewById(R.id.moneda);
-        botonSumaTotal = (Button) findViewById(R.id.botonSumarTotal);
-        botonSumarAutoTotal = (Button) findViewById(R.id.botonSumarAutoTotal);
-        botonSumarAutoSpeedTotal = (Button) findViewById(R.id.botonSumarAutoSpeedTotal);
+
         textValorClick = (TextView) findViewById(R.id.textValorClick);
         textValorAutoClick = (TextView) findViewById(R.id.textValorAutoClick);
         textVelocidadAutoClick = (TextView) findViewById(R.id.textVelocidadAutoClick);
@@ -67,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
         moneda.startAnimation(fade_in);
     }
 
-    public void sumarAuto(){
+    public void sumarAuto() {
         new Thread(() -> {
-            while (true){
+            while (true) {
                 num = num.add(incAuto);
                 runOnUiThread(this::setContText);
                 try {
@@ -81,82 +84,74 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    public void setContText(){
+    public void setContText() {
 
 
-        if(num.longValue()>=1000000000){
-            numD = BigDecimal.valueOf(num.longValue()/1000000000d);
+        if (num.longValue() >= 1000000000) {
+            numD = BigDecimal.valueOf(num.longValue() / 1000000000d);
             contador.setText(df.format(numD) + "B");
-        }
-        else if(num.longValue()>=1000000){
-            numD = BigDecimal.valueOf(num.longValue()/1000000d);
-            contador.setText(df.format(numD) +"M");
-        }
-
-        else if(num.longValue()>=1000){
-            numD = BigDecimal.valueOf(num.longValue()/1000d);
-            contador.setText(df.format(numD) +"K");
-        }
-        else
+        } else if (num.longValue() >= 1000000) {
+            numD = BigDecimal.valueOf(num.longValue() / 1000000d);
+            contador.setText(df.format(numD) + "M");
+        } else if (num.longValue() >= 1000) {
+            numD = BigDecimal.valueOf(num.longValue() / 1000d);
+            contador.setText(df.format(numD) + "K");
+        } else
             contador.setText(num.toString());
-        botonSumaTotal.setText("100$ x" + num.divide(BigInteger.valueOf(100)));
-        botonSumarAutoTotal.setText("200$ x" + num.divide(BigInteger.valueOf(200)));
-        botonSumarAutoSpeedTotal.setText("400$ x" + num.divide(BigInteger.valueOf(400)));
+
         textValorClick.setText("Click: " + inc.toString());
-        textValorAutoClick.setText("Autoclick: "+ incAuto.toString());
-        textVelocidadAutoClick.setText("Autoclick speed: " + tiempoAutoClick + "m" +
-                "s");
+        textValorAutoClick.setText("Autoclick: " + incAuto.toString());
+        textVelocidadAutoClick.setText("Autoclick speed: " + tiempoAutoClick + "m" + "s");
     }
 
-    public void sumaTotal(View v){
+    public void sumaTotal(View v) {
         // inc += num/100 and num=num%100
         inc = inc.add(num.divide(BigInteger.valueOf(100)));
         num = num.remainder(BigInteger.valueOf(100));
         contador.setText(num.toString());
-        botonSumaTotal.setText("100$ x" + num.divide(BigInteger.valueOf(100)).toString());
+
     }
 
-    public void sumaTotalAuto(View v){
+    public void sumaTotalAuto(View v) {
         incAuto = incAuto.add(num.divide(BigInteger.valueOf(200)));
         num = num.remainder(BigInteger.valueOf(200));
         contador.setText(num.toString());
-        botonSumarAutoTotal.setText("200$ x" + num.divide(BigInteger.valueOf(200)).toString());
+
     }
 
-    public void incrementarAutoSpeed(View v){
-        if(num.longValue() >= 400) {
+    public void incrementarAutoSpeed(View v) {
+        if (num.longValue() >= 400) {
             tiempoAutoClick = (int) (tiempoAutoClick / 1.5);
             num = num.subtract(BigInteger.valueOf(400));
         }
     }
 
-    public void incrementarAutoSpeedTotal(View v){
-        if(num.longValue() >= 400){
+    public void incrementarAutoSpeedTotal(View v) {
+        if (num.longValue() >= 400) {
             int times = num.divide(BigInteger.valueOf(400)).intValue();
-            tiempoAutoClick = (int) (tiempoAutoClick/(1.5*times));
-        num = num.remainder(BigInteger.valueOf(400));
-        contador.setText(num.toString());
-        botonSumarAutoSpeedTotal.setText("400$ x" + times);
+            tiempoAutoClick = (int) (tiempoAutoClick / (1.5 * times));
+            num = num.remainder(BigInteger.valueOf(400));
+            contador.setText(num.toString());
         }
     }
 
-    public void incrementar(View v){
-        if(num.longValue() >= 100) {
+    public void incrementar(View v) {
+        if (num.longValue() >= 100) {
             inc = inc.add(BigInteger.valueOf(1));
             num = num.subtract(BigInteger.valueOf(100));
             contador.setText(num.toString());
         }
     }
 
-    public void incrementarAuto(View v){
-        if(num.longValue() >= 200){
+    public void incrementarAuto(View v) {
+        if (num.longValue() >= 200) {
             incAuto = incAuto.add(BigInteger.valueOf(1));
             num = num.subtract(BigInteger.valueOf(200));
             contador.setText(num.toString());
         }
     }
 
-    public void reset(View v){
+    public void reset(View v) {
         num = BigInteger.valueOf(0);
         contador.setText(num.toString());
     }
@@ -165,5 +160,14 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, PantallaInicio.class);
         startActivity(i);
 
+    }
+
+    public void irACompras(View view) {
+        Intent i = new Intent(this, Compras.class);
+        i.putExtra("MONEY_COUNT", num.toString());
+        i.putExtra("CLICK_VALUE", inc.toString());
+        i.putExtra("AUTOCLICK_VALUE", incAuto.toString());
+        i.putExtra("AUTOCLICK_TIME", tiempoAutoClick);
+        startActivity(i);
     }
 }
